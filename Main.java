65afb992086ba97405508418c146c342347504e0
Feras B A Y R Alshadad <83323@kfh.com>
2024-01-03 09:49:14 +0300

    Task Array Completed

---
+++ Main.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+  public static void main(String[] args) {
+    double[] WeatherTemps = {28.7, 34.7, 21.9, 31.9, 25.6};
+    WeatherTemps[2] = 24.7;
+    for (double Temp : WeatherTemps) {
+      System.out.println(Temp);
+  
+    
+  }
+  }
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
